b8962113fa5d62cb6d03b9f0abe8653710bdf70d
James Smith <jlsmith@gmail.com>
2009-06-04 12:25:43 -0400

    renamed test file.

--- test/default_serialize_options_test.rb
+++ test/default_serialize_options_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class DefaultToXmlTest < ActiveSupport::TestCase
+class DefaultSerializeOptionsTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
